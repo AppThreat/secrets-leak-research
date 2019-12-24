c177f6acfac615e08036d966419738bcdcfe685d
Prabhu Subramanian <prabhu@ngcloud.io>
2019-12-24 16:59:52 +0000

    Improvements

--- detect.sh
+++ detect.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
-ARGS="--disk --threads=1 --redact"
+ARGS="--threads=2"
 BIN_NAME="gitleaks_bin/gitleaks"
 
-mkdir -p gitleaks_bin reports
+mkdir -p repos gitleaks_bin reports
 curl -LO "https://github.com/zricethezav/gitleaks/releases/download/v3.0.3/gitleaks-linux-amd64"
 
 mv gitleaks-linux-amd64 $BIN_NAME
@@ -13,11 +13,17 @@ for i in `cat repo-list.txt`; do
   echo "Scanning $i"
   basename=$(basename $i)
   reponame=${basename%.*}
-  $BIN_NAME ${ARGS} -r ${i} --report=reports/${reponame}.json
-  cat reports/${reponame}.json | jq -c '.[]' > reports/${reponame}-l.json
+  if [ -d "repos/$reponame" ]; then
+    rm -rf "repos/$reponame"
+  fi
+  git clone --depth=20 $i repos/$reponame
+  $BIN_NAME ${ARGS} --report=reports/${reponame}.json --repo-path=repos/$reponame
+  if [ -f "${reponame}.json" ]; then
+    cat reports/${reponame}.json | jq -c '.[]' > reports/${reponame}-l.json
+    rm reports/${reponame}.json
+  fi
 done
 
+rm -rf repos gitleaks_bin
 cat reports/*-l.json > reports/full-report.json
-
-rm -rf gitleaks_bin
 ls -lh reports
